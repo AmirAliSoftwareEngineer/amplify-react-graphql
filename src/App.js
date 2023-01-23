1b9c8e592c00dea6a50b683b54a89d219eb50158
Amir Ali Software Engineer | Full Stack Developer | Cloud Native <73399849+AmirAliSoftwareEngineer@users.noreply.github.com>
2023-01-23 07:08:45 +0500

    changed break

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h1>Hello Moto this is our First React Native Application</h1>
         <br></br>
         <h2> Finally our new version is going to relase with latest update</h2>
+        <br></br>
         <h3>It's really great to see</h3>
         <span> that our CI/CD Working perfect</span>
       </header>
