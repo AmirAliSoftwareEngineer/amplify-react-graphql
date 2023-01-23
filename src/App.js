8abeac815f3f406d2571012f7bbffa47abe21b10
Amir Ali Software Engineer | Full Stack Developer | Cloud Native <73399849+AmirAliSoftwareEngineer@users.noreply.github.com>
2023-01-23 06:35:10 +0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -3,25 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-<<<<<<< HEAD
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello </h1>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
-=======
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Hello from V2</h1>
-            </header>
-        </div>
-    );
->>>>>>> b0c2bda (changes for v2)
 }
 
 export default App;
