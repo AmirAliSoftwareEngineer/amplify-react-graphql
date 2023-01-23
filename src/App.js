c786adc497e43994e611e9948c5ab1ad0870f5a3
Amir Ali Software Engineer | Full Stack Developer | Cloud Native <73399849+AmirAliSoftwareEngineer@users.noreply.github.com>
2023-01-23 07:02:17 +0500

    changes for latest version10

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello Moto this is our First React Native Application</h1>
+        <br></br>
+        <h2> Finally our new version is going to relase with latest update</h2>
       </header>
     </div>
   );
