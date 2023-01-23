4a82745304b096d6a471cbb1cd4616bc2ae23b8a
Amir Ali Software Engineer | Full Stack Developer | Cloud Native <73399849+AmirAliSoftwareEngineer@users.noreply.github.com>
2023-01-23 07:07:56 +0500

    updated final

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <h1>Hello Moto this is our First React Native Application</h1>
         <br></br>
         <h2> Finally our new version is going to relase with latest update</h2>
+        <h3>It's really great to see</h3>
+        <span> that our CI/CD Working perfect</span>
       </header>
     </div>
   );
